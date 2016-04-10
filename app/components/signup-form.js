e8561eec1444a36c305ffbf4f6802d7e59db6495
eddieduro <e.jamesduro@gmail.com>
2016-04-10 13:42:06 -0700

    update sign up form and user model

--- app/components/signup-form.js
+++ app/components/signup-form.js
@@ -5,8 +5,11 @@ export default Ember.Component.extend({
   actions: {
     register: function() {
       var params = {
+        firstName: this.get('firstName'),
+        lastName: this.get('lastName'),
         email:  this.get('email'),
-        password:  this.get('password')
+        password:  this.get('password'),
+        profilePicture: this.get('profilePicture')
       }
     this.sendAction('register', params);
     this.transitionTo('login');
--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr(),
+  firstName: DS.attr(),
+  lastName: DS.attr(),
   email: DS.attr(),
   password: DS.attr(),
-  ratings: DS.hasMany('rating', {async: true})
+  profilePicture: DS.attr()
 });
