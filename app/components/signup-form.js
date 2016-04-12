89f44411e51ed8a858e040e50c9323b75e02d1ad
eddie duro and michael lor <michael@>
2016-04-12 11:30:03 -0700

    alert users on successful/unsuccessful registration

--- app/components/signup-form.js
+++ app/components/signup-form.js
@@ -9,8 +9,21 @@ export default Ember.Component.extend({
         lastName: this.get('lastName'),
         email:  this.get('email'),
         password:  this.get('password'),
-        profilePicture: this.get('profilePicture')
+        profilePicture:  this.get('profilePicture') ? this.get('profilePicture'): "http://picturepan2.github.io/spectre/demo/img/avatar-1.png"
       }
+    console.log(this.get('users'));
+    this.get('users').every(function(user){
+      var userEmail = user.get('email');
+      console.log(userEmail, params.email);
+      if(userEmail === params.email) {
+        alert("Account with that email already exists!");
+        return false;
+      } else if (userEmail !== params.email) {
+        alert("Registration Successful!");
+        return false;
+      }
+    })
+
     this.sendAction('register', params);
     this.transitionTo('login');
       // var newUser = this.store.createRecord('user', {
--- app/routes/signup.js
+++ app/routes/signup.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('user');
+  },
   actions: {
     register: function(params) {
 
