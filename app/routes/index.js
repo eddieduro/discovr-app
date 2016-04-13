bd2b4f0c75f59ca7ecf903d655efa60e0278a82c
eddie duro and michael lor <e.jamesduro@gmail.com>
2016-04-13 15:51:39 -0700

    update image grid on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,17 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   session: Ember.inject.service(),
-
   model(){
     return Ember.RSVP.hash({
       users: this.store.findAll('user'),
-      pictures: this.store.findAll('picture')
+      pictures: this.store.query('picture', { limitToFirst: 3, startsAt: 0})
     })
   },
   actions: {
     transitionToProfile(sessionUser) {
       this.transitionTo('profile', sessionUser);
     }
-
-  }
+  },
 });
